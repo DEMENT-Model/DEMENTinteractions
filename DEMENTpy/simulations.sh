0316732f53bee5cc91a9d54929c0aa3edc2d7c3a
Brittni Bertolet <brittnibertolet@gmail.com>
2024-09-03 15:46:52 -0400

    Update simulations.sh
    Added seed numbers for additional 40 simulations conducted

--- DEMENTpy/simulations.sh
+++ DEMENTpy/simulations.sh
@@ -4,7 +4,7 @@
 for i in input/ambient/ input/drought/ input/moist
 do
 	# Loop through 10 different microbial initializations, using random number seeds
-	for j in 12089 22765 26152 3365 4325 440 5151 5192 5732 8104
+	for j in 12089 22765 26152 3365 4325 440 5151 5192 5732 8104 13089 22775 25152 335 4305 410 5051 5792 572 8141 1089 2275 2512 35 4105 100 5451 9200 5882 8417 11089 27765 29152 4365 432 7740 1510 1923 5932 1014 1209 2765 30152 43265 4132 3740 1210 1023 5935 1114
 	do
 		# Submit JOB-fullsims.sh to run full simulation
 		export i; export j
